553dfc09157115117eaebf64ceb2127da1dd3137
MohammadMahdi <mohamad.mahdi.eng98@gmail.com>
2024-07-27 02:41:32 +0330

    working on home page of cv

--- app/page.tsx
+++ app/page.tsx
@@ -2,87 +2,83 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+    <div className="flex max-w-8xl w-full h-full justify-between">
+      <div className="basis-2/3 w-full">
+        <div className="flex w-full justify-between px-1 my-10">
+          <h1 className="font-vazirmatn text-3xl text-white font-black">
+            محمد مهدی
+          </h1>
+          <h1 className="text-white text-3xl font-black">Mohammad Mahdi</h1>
+        </div>
+        <p className="text-right text-white font-vazirmatn text-xl my-10 leading-loose">
+          برنامه‌نویس فرانت‌اند با بیش از 1 سال تجربه در توسعه و طراحی
+          وب‌سایت‌های حرفه‌ای با استفاده از HTML، CSS، JavaScript و فریمورک های
+          React و Next. علاقمند به یادگیری فناوری‌های جدید و بهبود رابط کاربری
+          با طراحی‌های زیبا و کاربردی. دارای توانایی کار تیمی و حل مسائل به شکل
+          خلاقانه.
+        </p>
+        <div className="flex flex-col gap-6 text-white px-2 my-10">
+          <p className="text-2xl font-vazirmatn font-bold">مهارت ها: </p>
+          <div className="flex flex-wrap gap-4">
+            <span className="border rounded-lg py-1 px-3 bg-gray-500/30">
+              HTML
+            </span>
+            <span className="border rounded-lg py-1 px-3 bg-gray-500/30">
+              Css
+            </span>
+            <span className="border rounded-lg py-1 px-3 bg-gray-500/30">
+              JavaScript
+            </span>
+            <span className="border rounded-lg py-1 px-3 bg-gray-500/30">
+              React
+            </span>
+            <span className="border rounded-lg py-1 px-3 bg-gray-500/30">
+              Next
+            </span>
+            <span className="border rounded-lg py-1 px-3 bg-gray-500/30">
+              Tailwind
+            </span>
+            <span className="border rounded-lg py-1 px-3 bg-gray-500/30">
+              Sass
+            </span>
+            <span className="border rounded-lg py-1 px-3 bg-gray-500/30">
+              Bootstrap
+            </span>
+            <span className="border rounded-lg py-1 px-3 bg-gray-500/30">
+              Redux
+            </span>
+            <span className="border rounded-lg py-1 px-3 bg-gray-500/30">
+              TypeScript
+            </span>
+            <span className="border rounded-lg py-1 px-3 bg-gray-500/30">
+              Leaflet
+            </span>
+            <span className="border rounded-lg py-1 px-3 bg-gray-500/30">
+              Git
+            </span>
+            <span className="border rounded-lg py-1 px-3 bg-gray-500/30">
+              GitFlow
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col gap-6 text-white px-w">
+          <p className="text-2xl font-bold font-vazirmatn">سوابق تحصیلی :</p>
+          <div className="flex flex-col font-vazirmatn border-r-4 px-2">
+              <p className="text-lg">مهندسی کامپیوتر | نرم افزار</p>
+              <p className="text-lg">دانشگاه هرمزگان</p>
+              <p className="text-lg">1389 - 1403</p>
+          </div>
+        </div>
+      </div>
+      <div className="basis-1/3 flex flex-col justify-center w-full">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/profile.png"
+          className="rounded-full border-4 border-gray-500/30 mx-auto"
+          height={350}
+          width={350}
+          alt="Profile photo"
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </>
+    </div>
   );
 }
